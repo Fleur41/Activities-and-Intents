8bbf2eb95347b406c105c9455f7e25fe8a501441
mac <@Fleur41>
2025-07-31 12:55:40 +0300

    updated activities and created explicit intents

--- app/src/main/java/com/sam/activitiesandintents/MainActivity.kt
+++ app/src/main/java/com/sam/activitiesandintents/MainActivity.kt
@@ -1,28 +1,20 @@
 package com.sam.activitiesandintents
 
-import android.R.attr.button
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.sam.activitiesandintents.ui.theme.ActivitiesAndIntentsTheme
+
 
 class MainActivity : ComponentActivity() {
     var counter: Int = 0
     lateinit var textView: TextView
     lateinit var button: Button
     lateinit var launchActivityB: Button
+    lateinit var launchBrowser: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -30,6 +22,7 @@ class MainActivity : ComponentActivity() {
         textView = findViewById(R.id.textView)
         button = findViewById(R.id.button)
         launchActivityB = findViewById(R.id.launchActivityB)
+        launchBrowser = findViewById(R.id.launchBrowser)
 
         button.setOnClickListener {
             counter++
@@ -39,6 +32,28 @@ class MainActivity : ComponentActivity() {
         launchActivityB.setOnClickListener {
             val intent = Intent(this, ActivityB::class.java)
             startActivity(intent)
+        }
+
+        launchBrowser.setOnClickListener {
+            val url = "https://www.google.com"
+            //method 1
+            try {
+                val intent = Intent(Intent.ACTION_VIEW).apply {
+                    data = Uri.parse(url)
+                }
+                startActivity(intent)
+            } catch (e: Exception) {
+//                TODO("Not yet implemented")
+                Toast.makeText(this, "Failed to open webpage", Toast.LENGTH_SHORT).show()
+            }
+
+//            method 2
+//            val intent = Intent(Intent.ACTION_VIEW).apply {
+//                data = Uri.parse(url)
+//            }
+//            if (intent.resolveActivity(packageManager) != null) {
+//                startActivity(intent)
+//            }
         }
     }
 
